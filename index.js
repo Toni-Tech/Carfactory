e02a3fc0b98ef3a49960531e9bf90010fee97499
Toni Thompson <careers.toni@gmail.com>
2023-05-24 14:40:08 -0700

    New project

---
+++ index.js
@@ -0,0 +1,31 @@
+// Declaration
+class Car {
+    constructor(make, model, year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+    
+    getDescription() {
+        return 'This is a ${this.year} ${this.make} ${this.model}';
+    }
+} 
+
+class ElectricCar extends Car {
+    constructor(make, model, year, range) {
+        super(make, model, year);
+    }
+    
+    getDescription() {
+            return 'This is a ${this.year} ${this.make} ${this.model} with a range of ${this.range} miles.';
+    }
+}
+
+const myCar = new ElectricCar("Tesla", "Model S", 2019, 300);
+console.log(myCar.getDescription()); 
+
+
+
+
+
+
